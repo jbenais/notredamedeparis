9e9d1275fcf5f9534b5b6b4b6cda4299d7eff438
Yoan Feijoeiro <yoan.feijoeiro@epita.fr>
2018-02-07 01:13:17 +0100

    [LEVEL 0] add animations (player)

--- Notre Dame de Paris/Assets/Scripts/PlayerController.cs
+++ Notre Dame de Paris/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,25 @@ public class PlayerController : MonoBehaviour {
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
+	private Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+	}
 
 	void Update()
 	{
 		CharacterController controller = GetComponent<CharacterController>();
 		var z = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", z);
+		if (z > 0)
+			transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+		else if (z < 0)
+			transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
 
 		if (controller.isGrounded) {
-			moveDirection = new Vector3 (0, 0, z);
+			moveDirection = new Vector3 (0, 0, Mathf.Abs(z));
 			moveDirection = transform.TransformDirection (moveDirection);
 			moveDirection *= speed;
 			if (Input.GetButton ("Jump"))
