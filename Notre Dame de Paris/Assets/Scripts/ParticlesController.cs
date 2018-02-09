534164101547f5d5cf19c79227d1e3dea5fda8a3
Julia Benais <jbenais@csumb.edu>
2018-02-09 12:10:06 +0100

    MISC: Deleted all trash fles

--- Notre Dame de Paris/Assets/Scripts/ParticlesController.cs
+++ Notre Dame de Paris/Assets/Scripts/ParticlesController.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticlesController : MonoBehaviour {
-
 	// Use this for initialization
+	float timer;
+	bool play = true;
+
 	void Start () {
-		
+		timer = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (timer >= 5f) {
+			if (play) {
+				this.gameObject.GetComponent<ParticleSystem> ().Stop ();
+			} else {
+				this.gameObject.GetComponent<ParticleSystem> ().Play ();
+			}
+			timer = 0f;
+		}
+		play = !play;
+		timer += Time.deltaTime;
 	}
 
-	void OnParticleCollision (GameObject collision) {
-		Debug.Log ("OKKKK!"); 
-		//if (collision.transform.tag == "player") {
-		//	Debug.Log ("Joueur touché !");
-		//}
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player" && play) {
+			other.gameObject.GetComponent<Player3DController> ().die ();
+		}
 	}
 }
