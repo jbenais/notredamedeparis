86bb181d126ee71da2c630fd3c2ba0ac25a27cb4
Yoan FEIJOEIRO <yoan.feijoeiro@epita.fr>
2018-02-11 19:29:24 +0100

    [MENU] add menu

---
+++ Notre Dame de Paris/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+	public void StartGame() {
+		SceneManager.LoadScene ("Level_0");
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
