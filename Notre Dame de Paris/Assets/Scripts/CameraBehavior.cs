34e1a583cab1b4430c09200306fb8a37078ed129
Yoan FEIJOEIRO <yoan.feijoeiro@epita.fr>
2018-02-11 14:36:01 +0100

    [LEVEL_0] add camera rotation to the right

--- Notre Dame de Paris/Assets/Scripts/CameraBehavior.cs
+++ Notre Dame de Paris/Assets/Scripts/CameraBehavior.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour {
-
 	public Transform playerPos;
+	public Transform[] checkPoints;
+	public float speed = 5f;
+	public bool moveRight = false;
+
 	private Vector3 floorOne;
 	private Vector3 floorTwo;
 	private Vector3 velocity;
+	private int step = 0;
 
 	void Start() {
 		floorOne = transform.position;
@@ -17,6 +21,8 @@ public class CameraBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		checkFloor ();
+		if (moveRight)
+			moveToRight ();
 	}
 
 	void checkFloor() {
@@ -26,6 +32,16 @@ public class CameraBehavior : MonoBehaviour {
 		else if (playerPos.position.y < 8.8f && transform.position.y > floorOne.y + 0.5f)
 			transform.position = Vector3.SmoothDamp (transform.position, floorOne, ref velocity, 1f);
 	}
+
+	void moveToRight() {
+		if (step < checkPoints.Length) {
+			transform.position = Vector3.MoveTowards (transform.position, checkPoints[step].position, speed * Time.deltaTime);
+			transform.LookAt (new Vector3 (0, 5.5f, 25));
+			if (transform.position == checkPoints [step].position)
+				step++;
+		}
+	}
+
 }
 
 
